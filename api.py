7fe50d2f324d4825d629d6a95758929a3dd71b04
gadiel <gadielyanelli221@gmail.com>
2025-11-07 11:08:37 -0300

    puede ser pa

--- api.py
+++ api.py
@@ -1,27 +1,58 @@
-from fastapi import FastAPI
-from carritoGLY import carrito, producto, cliente
+from fastapi import FastAPI, Depends
+from carritoGLY import carrito, producto, cliente,  getCursor, psycopg, pedido
+
 
 app = FastAPI(title="carrito de compra")
 cursor_Carrito = carrito()
 
-@app.get("/carrito")
-def ver_productos():
-    return cursor_Carrito.mostrar_productos()
+@app.get("/productos")
+def ver_productos(cursor: psycopg.Cursor = Depends(getCursor)):
+    return cursor_Carrito.mostrarProductos(cursor)
 
-@app.post("/guardar")
-def guardar_producto(cliente1: cliente, producto1: producto):
-    cursor_Carrito.baseDeDatos(cliente1, producto1)
-    return {"mensaje": "se guardo"}
+@app.get("/pedido")
+def ver_pedido(cursor: psycopg.Cursor = Depends(getCursor)):
+    return cursor_Carrito.mostrarPedido(cursor)
+
+@app.get("/Cliente")
+def ver_cliente(cursor: psycopg.Cursor = Depends(getCursor)):
+    return cursor_Carrito.mostrarCliente(cursor)
+
+
+@app.get("/pedidos")
+def ver_pedidos(cursor: psycopg.Cursor = Depends(getCursor)):
+    return cursor_Carrito.mostrarPedido(cursor)
+
+@app.post("/insertarPedido")
+def insertarElPedido(pedido1: pedido, cursor: psycopg.Cursor = Depends(getCursor)):
+    return cursor_Carrito.insertarPedido(pedido1,cursor)
+
+@app.post("/agregarCliente")
+def agregar_Cliente(cliente: cliente, cursor: psycopg.Cursor = Depends(getCursor)):
+    return cursor_Carrito.agregarCliente(cliente, cursor)
 
 @app.post("/agregarProducto")
-def agregar_producto(producto1: producto):
-    cursor_Carrito.agregar_productos(producto1)
-    return {"mensaje": f"Producto {producto1.nomPRODUCTOS} agregadd"}
+def agregar_producto(producto1: producto, cursor: psycopg.Cursor = Depends(getCursor)):
+    return cursor_Carrito.agregarProducto(producto1, cursor)
 
-@app.delete("/eliminar/{id_cliente}")
-def eliminar_cliente(id_cliente: int):
-    return cursor_Carrito.eliminar_cliente(id_cliente)
 
-@app.put("/modificar/{id_cliente}")
-def modificar_cliente(id_cliente: int, cliente1: cliente):
-    return cursor_Carrito.modificar_cliente(id_cliente, cliente1)
+
+@app.delete("/eliminarCliente/{id_cliente}")
+def eliminar_cliente(id_cliente: int, cursor: psycopg.Cursor = Depends(getCursor)):
+    return cursor_Carrito.eliminarCliente(id_cliente, cursor)
+
+@app.delete("/eliminarProducto/{id_producto}")
+def eliminar_producto(id_producto: int, cursor: psycopg.Cursor = Depends(getCursor)):
+    return cursor_Carrito.eliminarProducto(id_producto, cursor)
+
+@app.delete("/eliminarPedido/{id_pedido}")
+def eliminar_pedido(id_pedido: int, cursor: psycopg.Cursor = Depends(getCursor)):
+    return cursor_Carrito.eliminarPedido(id_pedido, cursor)
+
+@app.put("/modificarCliente/{id_cliente}")
+def modificar_cliente(id_cliente: int, cliente1: cliente, cursor: psycopg.Cursor = Depends(getCursor)):
+    return cursor_Carrito.modificarCliente(id_cliente, cliente1, cursor)
+
+
+@app.put("/modificarProducto/{id_producto}")
+def modificar_producto(id_producto: int, producto1: producto, cursor: psycopg.Cursor = Depends(getCursor)):
+    return cursor_Carrito.modificarProducto(id_producto, producto1, cursor)
--- carritoGLY.py
+++ carritoGLY.py
@@ -1,13 +1,27 @@
-import os 
+from typing import Generator
+import os
 from dotenv import load_dotenv
 import psycopg
 from pydantic import BaseModel
 
-
 load_dotenv()
 password = os.getenv("passwords")
-# definimos las clases
 
+url = f"postgresql://postgres.slcwqkacolixtbssmhdx:{password}@aws-1-us-east-2.pooler.supabase.com:6543/postgres"
+
+
+def getCursor() -> Generator[psycopg.Cursor, None, None]:
+    conn = psycopg.connect(url, sslmode="require")
+
+    cursor = conn.cursor()
+    try:
+        yield cursor
+        conn.commit()
+    finally:
+        cursor.close()
+        conn.close()
+nombrecliente = ""
+# Definimos las clases
 class cliente(BaseModel):
     nombre: str
     apellido: str
@@ -16,74 +30,77 @@ class producto(BaseModel):
     nomPRODUCTOS: str
     precio: float
 
+class pedido(BaseModel):
+    id_pedido: int
+    id_producto: int
+    id_cliente: int
+
+
 class carrito:
-    def __init__(self):
-        self.productos = []
 
-    # agregamos productos al carrito
-    def agregar_productos(self, productos1: producto):
-        self.productos.append(productos1)
-
-    # get de los productos que agregamos
-    def mostrar_productos(self):
-        total = sum(p.precio for p in self.productos)
-        return {
-            "productos": [p.dict() for p in self.productos],
-            "total": total
-        }
-
-    #eliminar usuario en bd
-    
-    def eliminar_cliente(self, id_cliente: int):
-        conn = psycopg.connect(
-            dbname="clienteProducto",
-            user="tralalero",
-            password= "+passwords",
-            host="localhost",
-            port="5432"
-        )
-        cursor = conn.cursor()
+    def eliminarCliente(self, id_cliente: int, cursor):
         cursor.execute("DELETE FROM cliente WHERE id_cliente = %s", (id_cliente,))
-        conn.commit()
-        cursor.close()
-        conn.close()
         return {"mensaje": f"Cliente {id_cliente} eliminado"}
-    
-    #modificar usuario en bd
 
-    def modificar_cliente(self, id_cliente: int, cliente1: cliente):
-        conn = psycopg.connect(
-            dbname="clienteProducto",
-            user="tralalero",
-            password = "passwords",
-            host="localhost",
-            port="5432"
-        )
-        cursor = conn.cursor()
+    def eliminarProducto(self, id_producto: int, cursor): 
+        cursor.execute("DELETE FROM productos WHERE id_producto = %s", (id_producto,))
+        return {"mensaje": f" Producto {id_producto} eliminado"}
+
+    def eliminarPedido(self, id_pedido: int, cursor):
+        cursor.execute("DELETE FROM pedido WHERE id_pedido = %s", (id_pedido,))
+        cursor.commit()
+        return {"mensaje": f"pedido {id_pedido} eliminado"}
+
+    def mostrarProductos(self, cursor):
+        cursor.execute("SELECT id_producto, nombre, precio FROM productos")
+        for item in cursor:
+            return item
+        
+
+    def mostrarCliente(self, cursor):
+    
+        cliente = cursor.execute("SELECT id_cliente, nombre, apellido FROM cliente")
+        for item in cliente:
+            return item
+
+
+    def insertarPedido(self, pedido1: pedido, cursor):
+        cursor.execute("INSERT INTO pedido (id_pedido, id_cliente, id_producto) VALUES (%s, %s, %s)", (pedido1.id_pedido, pedido1.id_cliente, pedido1.id_producto))
+        return {"mensaje": "insertado"}
+    
+    def mostrarPedido(self, cursor):
+        cursor.execute("SELECT * FROM pedido INNER JOIN cliente ON id_cliente = cliente.id_cliente INNER JOIN productos ON id_producto = id_producto")
+        for item in cursor:
+            return item
+    
+
+    def modificarCliente(self, id_cliente: int, cliente1: cliente, cursor):
         cursor.execute(
             "UPDATE cliente SET nombre = %s, apellido = %s WHERE id_cliente = %s",
             (cliente1.nombre, cliente1.apellido, id_cliente)
         )
-        conn.commit()
-       
+
+      
         return {"mensaje": f"Cliente {id_cliente} actualizado"}
 
-    # BD de clientes insertar y relacionar
-    def baseDeDatos(self, cliente1: cliente, producto1: producto):
-        conn = psycopg.connect(
-            mibd="clienteProducto",
-            usuario="tralalero",
-            contraseña= "passwords",
-            host="localhost",
-            puerto="1234"
+    def agregarProducto(self, producto1: producto, cursor):
+        cursor.execute(
+            "INSERT INTO productos (nombre, precio) VALUES (%s, %s)",
+            (producto1.nomPRODUCTOS, producto1.precio)
         )
-        cursor = conn.cursor()
+    def agregarCliente(self, cliente1: cliente, cursor):
+        cursor.execute(
+            "INSERT INTO cliente (nombre, apellido) VALUES (%s, %s)",
+            (cliente1.nombre, cliente1.apellido)
+        )
 
-        cursor.execute("""CREATE TABLE IF NOT EXISTS cliente(id_cliente SERIAL PRIMARY KEY,nombre TEXT,apellido TEXT)""")
-        cursor.execute("""CREATE TABLE IF NOT EXISTS producto( id_producto SERIAL PRIMARY KEY, nombrePROD TEXT, precio NUMERIC)""")
-        cursor.execute("""CREATE TABLE IF NOT EXISTS pedido(id_pedido SERIAL PRIMARY KEY,id_cliente INTEGER REFERENCES cliente(id_cliente),id_producto INTEGER REFERENCES producto(id_producto), totalPRECIO NUMERIC)""")
-       
-        cursor.execute("INSERT INTO cliente (nombre, apellido) VALUES (%s, %s)",(cliente1.nombre, cliente1.apellido))
-        cursor.execute("INSERT INTO producto (nombrePROD, precio) VALUES (%s, %s)",(producto1.nomPRODUCTOS, producto1.precio))
 
-# recordatorio PC n°7
+        return {"mensaje": f"Producto '{cliente1.nombre}' agregado a la base de datos"}
+
+    def modificarProducto(self, id_producto: int, producto1: producto, cursor):
+        cursor.execute("UPDATE productos SET nombre = %s, precio = %s WHERE id_producto = %s",
+            (producto1.nomPRODUCTOS,producto1.precio,id_producto))
+
+
+        return {"mensaje": f"Cliente {id_producto} actualizado"}
+#MM, APAC,  MCMP,  ECEP
